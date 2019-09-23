72c5f87eacc7ff67aff18b4bb05ec978a2070290
nawabgit <nawab.mir14@gmail.com>
2019-09-23 02:04:04 +0100

    added kelly criterion calculator

--- src/simulator.py
+++ src/simulator.py
@@ -1,15 +1,18 @@
+from decimal import Decimal
+
+
 class BetSimulator:
 
-    def __init__(self, stake, samples, odds_diff, random=False):
+    def __init__(self, bankroll, samples, odds_diff, random=False):
         """
-        :param stake: initial stake for strategy
+        :param bankroll: initial stake for strategy
         :param samples: n number of bet attempts
         :param odds_diff: the maximum odds we will take advantages of
                          e.g. 2.1 betfair, 2.2 bookie = odds_diff of 0.1
         :param random: if odd_diff is a static value or a range
         """
 
-        self.stake = stake
+        self.bankroll = Decimal(bankroll)
         self.samples = samples
         self.odds_diff = odds_diff
         self.random = random
@@ -17,6 +20,20 @@ class BetSimulator:
     def simulate(self):
         pass
 
-    def kelly_criterion(self):
-        pass
+    def kelly_criterion(self, b, p, weight=0.5):
+        """
+        Applies the kelly criterion formula with a modest 1/2 (modifiable) weighting
+        :param b the decimal odds proposed by the bookie
+        :param p the 'real' odds of match from 0 to 1
+        :param weight the 'safeness' multiplier applied to reduce the risk
+
+        :return: the amount that should be staked on the bet
+        """
+
+        b = b - 1
+        q = 1 - p
+
+        weighted_kc = Decimal(weight) * Decimal(((b * p) - q) / b)
+
+        return round(self.bankroll * weighted_kc, 2)
 
